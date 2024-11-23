695669a59d91ce505720ae5f71721f175224022b
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-11-23 20:00:08 +0530

    Add files via upload

---
+++ GayuMusic/plugins/tools/ping.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, PING_IMG_URL
+from GayuMusic import app
+from GayuMusic.core.call import Gayu
+from GayuMusic.utils import bot_sys_stats
+from GayuMusic.utils.decorators.language import language
+from GayuMusic.utils.inline import support_group_markup
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    start = datetime.now()
+    pytgping = await Gayu.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(
+            resp,
+            app.mention,
+            UP,
+            RAM,
+            CPU,
+            DISK,
+            pytgping,
+        ),
+        reply_markup=support_group_markup(_),
+    )
